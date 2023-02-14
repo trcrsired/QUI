ede5774d1da7b4cb1d113c07fd72918398f49e62
trcrsired <uwgghhbcad@gmail.com>
2023-02-14 16:22:06 -0500

    aura

--- QUI_Aura/aura.lua
+++ QUI_Aura/aura.lua
@@ -1,6 +1,20 @@
 local QUI = LibStub("AceAddon-3.0"):GetAddon("QUI")
-local last_buff,last_debuff = 0,0
 
+if BuffFrame.auraFrames then
+local BuffFrame = BuffFrame
+
+local bf_auraframes = BuffFrame.auraFrames
+for i=1,#bf_auraframes do
+	bf_auraframes[i].Icon:SetTexCoord(0.1,0.9,0.1,0.9)
+end
+
+local dbf_auraframes = DebuffFrame.auraFrames
+for i=1,#dbf_auraframes do
+	dbf_auraframes[i].Icon:SetTexCoord(0.1,0.9,0.1,0.9)
+end
+
+else
+local last_buff,last_debuff = 0,0
 QUI:RegisterEvent("UNIT_AURA",function(event,unit)
 	if unit == PlayerFrame.unit then
 		local _G = _G
@@ -14,3 +28,5 @@ QUI:RegisterEvent("UNIT_AURA",function(event,unit)
 		last_debuff=DEBUFF_ACTUAL_DISPLAY
 	end
 end)
+	
+end
