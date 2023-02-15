988e7f38dbc90d0c09c8aa2658faaf135a478d51
trcrsired <uwgghhbcad@gmail.com>
2023-02-14 19:22:16 -0500

    macro

--- QUI_Macro/macro.lua
+++ QUI_Macro/macro.lua
@@ -1,8 +1,5 @@
-MacroFrame.NineSlice:Hide()
-MacroFrame.Bg:SetTexture(131071)
-MacroFrame.TitleBg:SetTexture(131071)
-MacroFrame.TopTileStreaks:SetAlpha(0)
-MacroFrameInset:Hide()
+local QUI = LibStub("AceAddon-3.0"):GetAddon("QUI")
+QUI.KillFrameNineSlice(MacroFrame)
 
 MacroHorizontalBarLeft:SetAlpha(0)
 
@@ -16,26 +13,19 @@ end
 
 
 local _G = _G
-for i=1,2 do
-	_G["MacroFrameTab"..i.."Left"]:SetAlpha(0)
-	_G["MacroFrameTab"..i.."Middle"]:SetAlpha(0)
-	_G["MacroFrameTab"..i.."Right"]:SetAlpha(0)
-	_G["MacroFrameTab"..i.."LeftDisabled"]:SetAlpha(0)
-	_G["MacroFrameTab"..i.."MiddleDisabled"]:SetAlpha(0)
-	_G["MacroFrameTab"..i.."RightDisabled"]:SetAlpha(0)
-end
+QUI.skin_dropdown("MacroFrameTab1")
+QUI.skin_dropdown("MacroFrameTab2")
+
+if MacroButton1 then
 
 local function skinmacrobutton(macroButtonName)
 	local macroButton = _G[macroButtonName]
-	local macroName = _G[macroButtonName.."Name"]
-	_G[macroButtonName.."Icon"]:SetTexCoord(0.1,0.9,0.1,0.9)
-	local regions = {macroButton:GetRegions()}
-	for i=1,#regions do 
-		local region = regions[i]
-		if region:IsObjectType("Texture") and region:GetTexture()=="Interface\\Buttons\\UI-EmptySlot-Disabled" then
-			region:SetAlpha(0)
-		end
+	local Icon = macroButton.Icon
+	if Icon == nil then
+		Icon = _G[macroButtonName.."Icon"]
 	end
+	Icon:SetTexCoord(0.1,0.9,0.1,0.9)
+	QUI.KillFrameBackgroundBySearch(macroButton,"Interface\\Buttons\\UI-EmptySlot-Disabled");
 end
 
 for i=1, max(MAX_ACCOUNT_MACROS, MAX_CHARACTER_MACROS) do
@@ -44,20 +34,18 @@ end
 
 skinmacrobutton("MacroFrameSelectedMacroButton")
 
+end
+
 MacroFrameSelectedMacroBackground:SetAlpha(0)
 
+if MacroButtonScrollFrameTop then
 MacroButtonScrollFrameTop:SetAlpha(0)
 MacroButtonScrollFrameMiddle:SetAlpha(0)
 MacroButtonScrollFrameBottom:SetAlpha(0)
 MacroButtonScrollFrameScrollBar:SetAlpha(0)
-
-local function skinbutton(button)
-	button.Left:SetAlpha(0)
-	button.Middle:SetVertexColor(0,0,0,1)
-	button.Right:SetAlpha(0)
-	button:SetHighlightTexture("")
 end
 
+local skinbutton = QUI.skin_button
 skinbutton(MacroEditButton)
 skinbutton(MacroCancelButton)
 skinbutton(MacroSaveButton)
