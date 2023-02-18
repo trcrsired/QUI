04d833567266111c0a36e88c6043d9ffd52a4a12
trcrsired <uwgghhbcad@gmail.com>
2023-02-18 00:19:13 -0500

    tradeskill

--- QUI_TradeSkill/tradeskill.lua
+++ QUI_TradeSkill/tradeskill.lua
@@ -1,17 +1,7 @@
-TradeSkillFrame.NineSlice:Hide()
-TradeSkillFrame.Bg:SetTexture(131071)
-TradeSkillFrame.TitleBg:SetTexture(131071)
-TradeSkillFrame.TopTileStreaks:Hide()
-TradeSkillFrame.RecipeInset:Hide()
+local QUI = LibStub("AceAddon-3.0"):GetAddon("QUI")
+QUI.KillFrameNineSlice(TradeSkillFrame)
 
-local function skin_tab(tab)
-	tab.Left:SetAlpha(0)
-	tab.Middle:SetAlpha(0)
-	tab.Right:SetAlpha(0)
-	tab.LeftDisabled:SetAlpha(0)
-	tab.MiddleDisabled:SetAlpha(0)
-	tab.RightDisabled:SetAlpha(0)
+if TradeSkillFrame.RecipeList then
+QUI.KillFrameLMRBorder(TradeSkillFrame.RecipeList.LearnedTab)
+QUI.KillFrameLMRBorder(TradeSkillFrame.RecipeList.UnlearnedTab)
 end
-
-skin_tab(TradeSkillFrame.RecipeList.LearnedTab)
-skin_tab(TradeSkillFrame.RecipeList.UnlearnedTab)
