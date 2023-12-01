d8de934c4e12c6591275e476cc1c36b0fcef392f
trcrsired <uwgghhbcad@gmail.com>
2023-12-01 17:20:08 -0500

    fix qui challenges

--- QUI_Challenges/challenges.lua
+++ QUI_Challenges/challenges.lua
@@ -1,6 +1,8 @@
+if ChallengesFrame_Update then
 hooksecurefunc("ChallengesFrame_Update", function(self)
 	local icons = self.DungeonIcons
 	for i=1,#icons do
 		icons[i]:GetRegions():SetAlpha(0)
 	end
 end)
+end
